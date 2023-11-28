71037a8b68656dced130964c50b2d7f869cd8d55
kvh888 <kv_h@mail.ru>
2023-11-28 21:42:54 +0700

    changed readme.md, add errors.txt

--- redis_get_started.py
+++ redis_get_started.py
@@ -4,11 +4,15 @@ redis = redis.StrictRedis(
     host='localhost',
     port='6379')
 
+'''Если не почистить базу, останутся удаленные элементы'''
+redis.flushdb()  # clear db
+
 redis.set('mykey', 'Hello from Python!')
 value = redis.get('mykey')
 print(value)
 
 redis.zadd('vehicles', {'car': 0})
 redis.zadd('vehicles', {'bike': 0})
+redis.zadd('vehicles', {'slon': 0})
 vehicles = redis.zrange('vehicles', 0, -1)
 print(vehicles)
